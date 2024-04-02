63c080888c8e0e3bf96863447c62459721c3b111
sheldonu <uch20002@byui.edu>
2024-04-02 10:42:45 -0700

    dom elements added

---
+++ script.js
@@ -0,0 +1,12 @@
+// get dom elements
+const resultElement = document.getElementById("result");
+
+const pokemonImageElement = document.getElementById("pokemonImage");
+
+const optionsMain = document.getElementById("options");
+
+const pointsElement = document.getElementById("pointsValue");
+
+const totalCount = document.getElementById("totalCount");
+
+const loadingMain = document.getElementById("loadingMain");
