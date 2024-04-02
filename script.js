372619dbc9cd916a0b683d835190689a1db8abc0
sheldonu <uch20002@byui.edu>
2024-04-02 10:55:36 -0700

    created fetch function for pokemon

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ const pointsElement = document.getElementById("pointsValue");
 const totalCount = document.getElementById("totalCount");
 
 const loadingMain = document.getElementById("loadingMain");
+
+
+// function to fetch one pokemon with ID
+async function fetchPokemonById() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    const data = await response.json();
+    return data;
+}
+
+
+// test function
+async function testFetch() {
+    const pokemon = await fetchPokemonById();
+    console.log(pokemon);
+}
+
+// call the test function
+testFetch();
